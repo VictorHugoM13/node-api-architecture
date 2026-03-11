a8a0fa8a0f44b6a1c21546751dff3f3418cca896
VitorHugoM13 <victor_osk8@hotmail.com>
2026-03-11 20:37:45 -0300

    study(database): implementa operações básicas de SQL e criação de tabela users

---
+++ database/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL
+);
+
+INSERT INTO users (name, email)
+VALUES ('Victor', 'victor@email.com');
+
+INSERT INTO users (name, email)
+VALUES ('Ana', 'ana@email.com');
+
+
+
---
+++ tests/queries.sql
@@ -0,0 +1,4 @@
+SELECT * from users;
+
+UPDATE users SET name = "Victor Hugo" WHERE id = 1;
+DELETE FROM users WHERE id = 3;
